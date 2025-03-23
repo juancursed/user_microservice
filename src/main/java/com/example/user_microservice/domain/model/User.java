6253bf0d6af7bc901172c3adea92faf1b2986016
Kevin Marin <kealmahe@gmail.com>
2025-03-23 18:40:03 -0500

    Reorganizacion de los models

--- src/main/java/com/example/user_microservice/domain/model/User.java
+++ src/main/java/com/example/user_microservice/domain/model/User.java
@@ -3,7 +3,11 @@ package com.example.user_microservice.domain.model;
 import jakarta.persistence.*;
 import java.util.Date;
 import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -18,73 +22,15 @@ public abstract class User {
     private String password;
     private Date bornDate;
     private String phone;
+    private String license;
+    private String vehicle;
+    private int accessLevel;
+    private String department;
+    private String city;
+    private String address;
+    private String postalCode;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    // Getters y Setters
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getBornDate() {
-        return bornDate;
-    }
-
-    public void setBornDate(Date bornDate) {
-        this.bornDate = bornDate;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
 }
