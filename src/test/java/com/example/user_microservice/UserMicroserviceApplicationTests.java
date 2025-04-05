63d7b543e655fc3acce876fe0860ac0da843de62
juanda <juanvalenciam1603@gmail.com>
2025-04-05 14:00:56 -0500

    Implementacion de un test

--- src/test/java/com/example/user_microservice/UserMicroserviceApplicationTests.java
+++ src/test/java/com/example/user_microservice/UserMicroserviceApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.user_microservice;
 
+import com.example.user_microservice.application.usecase.SaveUserUseCase;
+import com.example.user_microservice.domain.model.User;
+import com.example.user_microservice.domain.model.Role;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserMicroserviceApplicationTests {
 
+    @Autowired
+    private SaveUserUseCase saveUserUseCase;
+
     @Test
-    void contextLoads() {
+    void testCrearUsuario() throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        User nuevo = new User();
+        nuevo.setFirstName("Juan");
+        nuevo.setLastName("Trin");
+        nuevo.setEmail("juantrin@gmail.com");
+        nuevo.setPassword("Wosito123"); // >= 8 caracteres
+        nuevo.setBornDate(sdf.parse("1990-01-01"));
+        nuevo.setPhone("+1234567890");
+        nuevo.setAccessLevel(1);
+        nuevo.setDepartment("Valle del Cauca");
+        nuevo.setCity("Tuluá york");
+        nuevo.setAddress("Calle Falsa 123");
+        nuevo.setPostalCode("12345");
+        nuevo.setRole(Role.CLIENT);
+
+        //----------Guardamos el Usuario test-----------
+        User creado = saveUserUseCase.execute(nuevo);
+        assertNotNull(creado);
+        assertNotNull(creado.getId());
+        assertEquals("Juan", creado.getFirstName());
+        assertEquals(Role.CLIENT, creado.getRole());
     }
 
 }
