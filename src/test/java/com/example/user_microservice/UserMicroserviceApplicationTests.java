693b2eef64ed66732e9b9f728294c6e39b8803f5
juanda <juanvalenciam1603@gmail.com>
2025-04-25 13:43:22 -0500

    add applcation.properties

--- src/test/java/com/example/user_microservice/UserMicroserviceApplicationTests.java
+++ src/test/java/com/example/user_microservice/UserMicroserviceApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,6 +18,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class UserMicroserviceApplicationTests {
 
